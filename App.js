ac5d877dc7db99de897800188dd0042e6d3680a4
kritika243 <24kritikasharma24@gmail.com>
2023-05-30 13:03:23 +0530

    margin gor status bar issue in android due to SafeAreaView

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+  const isAndroid = Platform.OS === 'android'
+  console.log(StatusBar.currentHeight)
   return (
     <>
-    <SafeAreaView style={{flex: 1}}>
-<View style={{backgroundColor:"green", padding: 16}}>
-  <Text>
-    Search 
-  </Text>
-</View>
-<View style={{backgroundColor:"blue", flex:1, padding: 16}}>
-  <Text>List</Text>
-</View>
-    </SafeAreaView>
-    <StatusBar style='auto' />
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 }}>
+        <View style={{ backgroundColor: "green", padding: 16 }}>
+          <Text>
+            Search
+          </Text>
+        </View>
+        <View style={{ backgroundColor: "blue", flex: 1, padding: 16 }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
     </>
   );
 }
 
 const styles = StyleSheet.create({
- 
+
 });
