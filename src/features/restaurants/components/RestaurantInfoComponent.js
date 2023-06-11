1a672aa09403ea74ab8b0f7c9d8ebc35dde7e964
kritika243 <24kritikasharma24@gmail.com>
2023-06-11 18:14:39 +0530

    open/closed restaurant info card

--- src/features/restaurants/components/RestaurantInfoComponent.js
+++ src/features/restaurants/components/RestaurantInfoComponent.js
@@ -10,6 +10,9 @@ const RestaurantCard = styled(Card)`
 const RestaurantCardCover = styled(Card.Cover)`
   backgroundColor:${(props) => props.theme.colors.bg.primary};
   padding: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px
+  
 `
 
 const Info = styled.View`
@@ -22,15 +25,29 @@ const RestaurantName = styled(Text)`
  font-size: ${(props) => props.theme.fontSizes.body}
 `
 
+const RatingAndStatus = styled.View`
+  display: flex;
+  flex-direction: row;
+  justifyContent: space-between
+`
+
 const RatingRow = styled.View`
   flex-direction: row;
   padding-top: 5px;
   padding-bottom: 5px;
 `
 
+const RestaurantStatus = styled.View`
+
+`
 const StarImage = styled.Image`
 width: 20px;
 height: 20px
+`
+
+const StatusImage = styled.Image`
+width: 25px;
+height: 25px
 `
 
 const RestaurantAddress = styled.Text`
@@ -43,19 +60,29 @@ const RestaurantAddress = styled.Text`
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const { name, icon, photos, address, isOpen, rating, isClosedTemporarily } = restaurant
   const ratingArray = Array.from(new Array(Math.floor(rating)))
+
+
   return <>
     <RestaurantCard elevation={5} >
       <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
       <Card.Content>
         <Info>
           <RestaurantName variant="titleLarge" >{name}</RestaurantName>
-          <RatingRow>
-            {
-              ratingArray.map((item, index) => (
-                <StarImage source={require('../../../../assets/star.png')} key={index} />
-              ))
-            }
-          </RatingRow>
+          <RatingAndStatus>
+            <RatingRow>
+              {
+                ratingArray.map((item, index) => (
+                  <StarImage source={require('../../../../assets/star.png')} key={index} />
+                ))
+              }
+            </RatingRow>
+            <RestaurantStatus>
+              {
+                isOpen ? <StatusImage source={require('../../../../assets/open.png')} /> : <StatusImage source={require('../../../../assets/closed.png')} />
+              }
+            </RestaurantStatus>
+          </RatingAndStatus>
+
 
           <RestaurantAddress>{address}</RestaurantAddress>
         </Info>
--- src/features/restaurants/screens/restaurantsScreen.js
+++ src/features/restaurants/screens/restaurantsScreen.js
@@ -32,10 +32,14 @@ export const RestaurantsScreen = () => {
           onChangeText={() => { }}
           value={''}
         />
-
       </SearchContainer>
+
       <ListContainer>
+
         <RestaurantInfo restaurant={{ name: "Restaurant 1", icon: '', photos: ['https://images.unsplash.com/photo-1515669097368-22e68427d265?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80'], address: 'random address', isOpen: true, rating: 4, isClosedTemporarily: false }} />
+
+
+
       </ListContainer>
     </MainContainer>
     <ExpoStatusBar style='auto' />
