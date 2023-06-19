f6018ce1ad11e2b1e0153ee2aed4b2f90285ef0c
kritika243 <24kritikasharma24@gmail.com>
2023-06-19 22:21:40 +0530

    :zap: screenOptions code improved

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Lato_300Light, Lato_400Regular, useFonts as useLato } from '@expo-google-fonts/lato';
 import { Oswald_400Regular, useFonts as useOswald } from '@expo-google-fonts/oswald';
+import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
@@ -23,13 +24,34 @@ export default function App() {
 
   if (!oswaldLoaded || !latoLoaded) return null
 
+
+  const TabIcon = {
+    Restaurants: 'restaurant-outline',
+    Maps: 'md-map-outline',
+    Settings: 'md-settings-outline'
+  }
+
+
+
+  const getScreenOptions = ({ route }) => {
+    const iconName = TabIcon[route.name]
+    return {
+      tabBarIcon: ({ size, color }) => (
+        <Ionicons name={iconName} size={size} color={color} />
+      ),
+      headerShown: false,
+      tabBarActiveTintColor: '#db5805',
+      tabBarInactiveTintColor: 'gray',
+    }
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator screenOptions={{
-            headerShown: false
-          }}>
+          <Tab.Navigator
+            screenOptions={getScreenOptions}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Maps" component={MapsScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
