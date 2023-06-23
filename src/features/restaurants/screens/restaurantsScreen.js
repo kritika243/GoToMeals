d5b61a8fdf3efd88eeecfedd46f642fd1ae85b3e
kritika243 <24kritikasharma24@gmail.com>
2023-06-23 18:14:39 +0530

    :sparkles: loader component while loading

--- src/features/restaurants/screens/restaurantsScreen.js
+++ src/features/restaurants/screens/restaurantsScreen.js
@@ -2,6 +2,7 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React, { useContext } from "react";
 import { FlatList, View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 import styled from "styled-components/native";
 
 import { MainContainer } from '../../../components/safearea-component';
@@ -11,6 +12,13 @@ import { RestaurantInfo } from "../components/RestaurantInfoComponent";
 
 const SearchContainer = styled(View)`
   padding: 16px;
+`
+
+const LoaderContainer = styled(View)`
+  display: flex;
+  /* justify-content: center;
+  align-items: center; */
+  margin: auto auto;
 `
 
 const RestaurantList = styled(FlatList).attrs({
@@ -24,27 +32,36 @@ export const RestaurantsScreen = () => {
 
   return <>
     <MainContainer>
-      <SearchContainer>
-        <Searchbar
-          placeholder="Search"
-          onChangeText={() => { }}
-          value={''}
-        />
-      </SearchContainer>
+      {
+        isLoading ? <>
+          <LoaderContainer>
+            <ActivityIndicator animating={true} color={MD2Colors.red800} size={'large'} />
+          </LoaderContainer>
+        </> : <>
+          <SearchContainer>
+            <Searchbar
+              placeholder="Search"
+              onChangeText={() => { }}
+              value={''}
+            />
+          </SearchContainer>
 
-      <RestaurantList
-        data={restaurants}
-        renderItem={({ item }) =>
-        (
-          <>
-            <Spacer position='bottom' size='large'>
-              <RestaurantInfo restaurant={item}
-              />
-            </Spacer>
-          </>
-        )
-        }
-        keyExtractor={(item) => item.name} />
+          <RestaurantList
+            data={restaurants}
+            renderItem={({ item }) =>
+            (
+              <>
+                <Spacer position='bottom' size='large'>
+                  <RestaurantInfo restaurant={item}
+                  />
+                </Spacer>
+              </>
+            )
+            }
+            keyExtractor={(item) => item.name} />
+        </>
+      }
+
 
     </MainContainer>
     <ExpoStatusBar style='auto' />
