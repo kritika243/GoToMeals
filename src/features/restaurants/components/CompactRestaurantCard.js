4ce4a026865be2457353153dd400eaf4381fbfbe
kritika243 <24kritikasharma24@gmail.com>
2023-07-05 21:29:44 +0530

    using WevView to render image in adroid as images not supported directly in android map callouts

--- src/features/restaurants/components/CompactRestaurantCard.js
+++ src/features/restaurants/components/CompactRestaurantCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Image, View } from 'react-native'
+import { Image, Platform, View } from 'react-native'
+import { WebView } from 'react-native-webview';
 import styled from 'styled-components/native'
 
 import { Text } from '../../../components/typography/text.component'
-
 
 const CompactItem = styled.View`
 padding: 10px ;
@@ -18,14 +18,21 @@ height: 100px
 margin-bottom: 10px
 
 `
+const CompactWebView = styled(WebView)`
+border-radius: 20px;
+width: 120px;
+height: 100px
+margin-bottom: 10px
 
+`
 
-
+const isAndroid = Platform.OS === 'android'
 const CompactRestaurantCard = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebView : CompactImage
   return (
     <>
       <CompactItem>
-        <CompactImage source={{ uri: restaurant.photos[0] }} />
+        <Image source={{ uri: restaurant.photos[0] }} />
         <Text variant="caption" center>
           {restaurant.name}
         </Text>
